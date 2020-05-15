4e5cebaa72622e2060faa73193f3aebdebf5dda6
Gautam Mishra <mishragautam96@gmail.com>
2020-05-15 23:23:24 +0530

    before rest template

--- movie-catalog-service/src/main/java/me/mgautam/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/me/mgautam/moviecatalogservice/resources/MovieCatalogResource.java
@@ -2,12 +2,14 @@ package me.mgautam.moviecatalogservice.resources;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.mgautam.moviecatalogservice.models.CatalogItem;
+import me.mgautam.moviecatalogservice.models.Rating;
 
 @RestController
 @RequestMapping("/catalog")
@@ -15,9 +17,14 @@ public class MovieCatalogResource {
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-        return Arrays.asList(
-            new CatalogItem("Titanic", "some titanic desc", 4)
+
+        List<Rating> ratings = Arrays.asList(
+            new Rating("1234", 4),
+            new Rating("5678", 3)
         );
+
+        return ratings.stream().map(rating -> new CatalogItem("Titanic", "some titanic desc", 4))
+                .collect(Collectors.toList());
     }
     
 }
