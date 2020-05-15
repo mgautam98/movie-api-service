1904636e702488cd4be036c6a1e67523c362742b
Gautam Mishra <mishragautam96@gmail.com>
2020-05-16 00:08:17 +0530

    add bean

--- movie-catalog-service/src/main/java/me/mgautam/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/me/mgautam/moviecatalogservice/resources/MovieCatalogResource.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +19,11 @@ import me.mgautam.moviecatalogservice.models.Rating;
 public class MovieCatalogResource {
 
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-
-        RestTemplate restTemplate = new RestTemplate();
 
         List<Rating> ratings = Arrays.asList(
             new Rating("1234", 4),
@@ -29,7 +31,7 @@ public class MovieCatalogResource {
         );
 
         return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8083/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
             return new CatalogItem(movie.getName(), "some titanic desc", rating.getRating());
         })
                 .collect(Collectors.toList());
