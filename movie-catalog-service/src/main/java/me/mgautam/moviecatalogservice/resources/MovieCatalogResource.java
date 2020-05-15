ff68d3477140ec79f63a94666e2d34aeb246ff40
Gautam Mishra <mishragautam96@gmail.com>
2020-05-15 23:33:34 +0530

    add rest template

--- movie-catalog-service/src/main/java/me/mgautam/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/me/mgautam/moviecatalogservice/resources/MovieCatalogResource.java
@@ -7,16 +7,23 @@ import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import me.mgautam.moviecatalogservice.models.CatalogItem;
+import me.mgautam.moviecatalogservice.models.Movie;
 import me.mgautam.moviecatalogservice.models.Rating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        restTemplate.getForEntity("http://localhost:8083/movies/foo", Movie.class);
 
         List<Rating> ratings = Arrays.asList(
             new Rating("1234", 4),
