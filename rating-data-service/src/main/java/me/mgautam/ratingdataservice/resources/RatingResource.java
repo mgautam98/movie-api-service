890c5736a7737a3047da11a56ffa0398532df4ca
Gautam Mishra <mishragautam96@gmail.com>
2020-05-15 22:19:25 +0530

    rating service

---
+++ rating-data-service/src/main/java/me/mgautam/ratingdataservice/resources/RatingResource.java
@@ -0,0 +1,17 @@
+package me.mgautam.ratingdataservice.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import me.mgautam.ratingdataservice.models.Rating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingResource {
+    
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+}
