2c8775defb6246908b96281142b3bca340220469
Gautam Mishra <mishragautam96@gmail.com>
2020-05-16 09:07:14 +0530

    integrate ratings api

--- rating-data-service/src/main/java/me/mgautam/ratingdataservice/resources/RatingResource.java
+++ rating-data-service/src/main/java/me/mgautam/ratingdataservice/resources/RatingResource.java
@@ -1,5 +1,8 @@
 package me.mgautam.ratingdataservice.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +16,13 @@ public class RatingResource {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/users/{userId}")
+    public List<Rating> getUserRatings(@PathVariable String userId){
+        return Arrays.asList(
+            new Rating("1234", 4),
+            new Rating("5678", 3)
+        );
     }
 }
